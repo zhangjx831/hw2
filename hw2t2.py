9e6266be7c3775e4c62140ea0860ce0d4602f27f
ShirleyGui <fraxinyue31@gmail.com>
2022-04-13 09:01:50 -0400

    update

--- hw2t2.py
+++ hw2t2.py
@@ -1,46 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
 
 import os
 os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages com.databricks:spark-xml_2.12:0.14.0 pyspark-shell'
-
-
-# In[2]:
-
 
 from pyspark.sql import SparkSession
 spark = SparkSession.builder.getOrCreate()
 df = spark.read.format('xml').options(rowTag='page').load('hdfs:/enwiki_test.xml')
 
-
-# In[3]:
-
-
-df.show()
-
-
-# In[4]:
-
-
-df.printSchema()
-
-
-# In[5]:
-
-
-df.select('revision.text._VALUE').show()
-
-
-# In[6]:
-
-
-df.select('title').show()
-
-
-# In[19]:
 
 
 import re
@@ -52,14 +20,16 @@ def extractLink(text):
     for res in results:
         if ':' in res and 'Category:' not in res:
             continue
-        elif '#' in res:
-            continue
         else:
-            output.append(res.split('|')[0].lower())
+            temp = res.split('|').lower() #all links
+            valid_links = [ x for x in temp if "#" not in x]
+            if len(valid_links)==0:
+                continue
+            else:
+                output.append(valid_links[0])
     return output
 
 
-# In[24]:
 
 
 from pyspark.sql.types import StringType, ArrayType
@@ -67,14 +37,6 @@ from pyspark.sql.types import StringType, ArrayType
 link_udf = udf(lambda text: extractLink(text), ArrayType(StringType()))
 newdf = df.withColumn("article", explode(link_udf(col("revision.text._VALUE"))))
 
-
-# In[25]:
-
-
-newdf.printSchema()
-
-
-# In[33]:
 
 
 newdf.select('title', 'article').repartition(1).write.option("delimiter", "\t").csv('test')
