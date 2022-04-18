28ef73fff826f98358d15643ba07cff1cdd139eb
jz3313 <jz3313@columbia.edu>
2022-04-18 18:23:51 +0000

    commit

--- hw2t2.py
+++ hw2t2.py
@@ -1,29 +1,29 @@
 import re
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import udf, col, explode
+from pyspark.sql.functions import udf, col, explode, lower
 from pyspark.sql.types import StringType, ArrayType
 
 spark = SparkSession.builder.getOrCreate()
-df = spark.read.format('xml').options(rowTag='page').load('hdfs://hw2-m/enwiki_whole.xml')
+df = spark.read.format('xml').options(rowTag='page').load('hdfs://hw2-m/enwiki_small.xml')
 def extractLink(text):
     try:
-        results = re.findall(r'(?<=\[\[).*?(?=\]\])', text)
+        results = regex.findall(r'\[\[((?:[^[\]]+|(?R))*+)\]\]', text)
     except:
         results = []
     output = []
     for res in results:
-        if ':' in res and 'Category:' not in res:
-            continue
-        else:
-            temp = res.split('|') #all links
-            valid_links = [ x.lower() for x in temp if "#" not in x]
-            if len(valid_links)==0:
+        for link in res.split('|'):
+            if ':' in link and 'Category:' not in link:
+                continue
+            elif '#' in link:
                 continue
             else:
-                output.append(valid_links[0])
+                output.append(link.lower())
+                break
     return output
 
 link_udf = udf(lambda text: extractLink(text), ArrayType(StringType()))
 newdf = df.withColumn("article", explode(link_udf(col("revision.text._VALUE"))))
-newdf.select('title', 'article').repartition(10).write.option("delimiter", "\t").csv('p2t2_whole')
+newdf = newdf.select(lower(col('title')).alias('title'), 'article')
+newdf.repartition(10).write.option("delimiter", "\t").csv('p2t2_small')
 
