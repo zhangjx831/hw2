738c7494022cbd05d4a8ac948caf6a0befb26efa
jz3313 <jz3313@columbia.edu>
2022-04-20 17:37:04 +0000

    commit

--- hw2t2.py
+++ hw2t2.py
@@ -4,7 +4,7 @@ from pyspark.sql.functions import udf, col, explode, lower
 from pyspark.sql.types import StringType, ArrayType
 
 spark = SparkSession.builder.getOrCreate()
-df = spark.read.format('xml').options(rowTag='page').load('hdfs://hw2-m/enwiki_small.xml')
+df = spark.read.format('xml').options(rowTag='page').load('hdfs://hw2-m/enwiki_whole.xml')
 def extractLink(text):
     try:
         results = regex.findall(r'\[\[((?:[^[\]]+|(?R))*+)\]\]', text)
@@ -25,5 +25,5 @@ def extractLink(text):
 link_udf = udf(lambda text: extractLink(text), ArrayType(StringType()))
 newdf = df.withColumn("article", explode(link_udf(col("revision.text._VALUE"))))
 newdf = newdf.select(lower(col('title')).alias('title'), 'article').orderBy('title', 'article')
-newdf.repartition(10).write.option("delimiter", "\t").csv('p2t2_small')
+newdf.repartition(10).write.option("delimiter", "\t").csv('p2t2_whole')
 
